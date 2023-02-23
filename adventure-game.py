120e0ec03e2aabfaf0ea8ebfce6e569461a9f60e
heyfai <nfaizahm@stackdefinition.com>
2023-02-24 00:40:30 +0400

    init commit

---
+++ adventure-game.py
@@ -0,0 +1,23 @@
+
+weapon = False
+name = input()
+
+
+def foyerScene():
+    directions = ["North", "South", "East", "West"]
+    print("You're in the hotel foyer."
+          "The high ceilings reflect luxury and wealth."
+          "Shame there's no one to marvel at it."
+          "Strangely, everything still works.")
+    print("There's a lot to do in here. What's first? North/South/East/West")
+    command = input()
+
+
+def liftLobby():
+    directions = ["North", "South", "East", "West"]
+    print("You're in the lift lobby. The lift's dead, though. Find a way to switch it on.")
+    print("As you look around, you find a flickering switch. Do you activate it? Yes/No")
+    command = input()
+
+
+
